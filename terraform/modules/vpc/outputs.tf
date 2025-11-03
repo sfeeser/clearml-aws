4a9ced6db8c479adc6cf370ff50767babe972e7b
Stuart Feeser <sfeeser@gmail.com>
2025-11-03 13:45:23 -0500

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,1 +1,9 @@
-
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "private_subnets" {
+  value = [] # Placeholder for actual subnet IDs
+}
+output "public_subnets" {
+  value = [] # Placeholder for actual subnet IDs
+}
