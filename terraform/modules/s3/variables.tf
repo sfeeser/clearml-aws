31cf3fee4803b45f3cb300312c83a5678f8fae0a
Stuart Feeser <sfeeser@gmail.com>
2025-11-04 09:52:16 -0500

    Update variables.tf

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -1,7 +1,8 @@
-
 variable "project_name" { type = string }
 variable "environment" { type = string }
 variable "region" { type = string }
-variable "bucket_names" { type = list(string) }
+variable "bucket_names" {
+  description = "List of required S3 buckets (e.g., artifacts, datasets, logs)"
+  type        = list(string)
+}
 variable "kms_key_alias" { type = string }
-
