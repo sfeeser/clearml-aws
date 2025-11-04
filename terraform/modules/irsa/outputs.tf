0cc1901e1e95f773db2b4b932f13ccb36298af31
Stuart Feeser <sfeeser@gmail.com>
2025-11-04 12:04:32 -0500

    Update outputs.tf

--- terraform/modules/irsa/outputs.tf
+++ terraform/modules/irsa/outputs.tf
@@ -1,6 +1,4 @@
-
-output "service_account_arn" {
-  value = aws_iam_role.irsa.arn
+output "clearml_fileserver_s3_policy_json" {
+  description = "The JSON string of the S3 access policy for the ClearML fileserver. Pass this to the generic IRSA module's 'policy_document' variable."
+  value       = data.aws_iam_policy_document.clearml_fileserver_s3_policy.json
 }
-
-
