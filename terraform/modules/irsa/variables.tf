aae22461fcd48bb5b5917d0186b58a0300c20873
Stuart Feeser <sfeeser@gmail.com>
2025-11-04 12:03:46 -0500

    Update variables.tf

--- terraform/modules/irsa/variables.tf
+++ terraform/modules/irsa/variables.tf
@@ -1,8 +1,23 @@
+# ----------------------------------------------------------------------------------
+# IRSA Module Variables
+# ----------------------------------------------------------------------------------
 
-variable "cluster_name" { type = string }
-variable "oidc_issuer" { type = string }
-variable "namespace" { type = string }
-variable "service_account" { type = string }
-variable "policy_document" { type = string } # JSON IAM policy from root module
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  type        = string
+}
 
+variable "eks_oidc_provider" {
+  description = "The OIDC provider URL of the EKS cluster (e.g., oidc.eks.us-east-1.amazonaws.com/id/EXAMPLEID)."
+  type        = string
+}
 
+variable "eks_oidc_provider_arn" {
+  description = "The full ARN of the EKS OIDC provider."
+  type        = string
+}
+
+variable "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket used by ClearML for artifact storage."
+  type        = string
+}
