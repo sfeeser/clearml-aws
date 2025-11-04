c23d6b7cfc1eb059bdcc1db675a652f3b42033c2
Stuart Feeser <sfeeser@gmail.com>
2025-11-04 08:09:54 -0500

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,6 +1,16 @@
-
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "vpc_cidr" { type = string }
-variable "region" { type = string }
-
+variable "project_name" {
+  description = "Project name for resource tagging"
+  type        = string
+}
+variable "environment" {
+  description = "Environment name for resource tagging"
+  type        = string
+}
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC (e.g., 10.0.0.0/16)"
+  type        = string
+}
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
