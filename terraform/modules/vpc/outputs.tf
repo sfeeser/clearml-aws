e5541a38c9cdffbfe8446d9b94e022221fc01c97
Stuart Feeser <sfeeser@gmail.com>
2025-11-04 08:10:55 -0500

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,11 +1,14 @@
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The VPC identifier"
+  value       = aws_vpc.main.id
 }
 output "private_subnets" {
-  value = [] # Placeholder for actual subnet IDs
+  description = "IDs of private subnets for EKS workers"
+  value       = [aws_subnet.private_a.id]
 }
 output "public_subnets" {
-  value = [] # Placeholder for actual subnet IDs
+  description = "IDs of public subnets for load balancers/NAT Gateway"
+  value       = [aws_subnet.public_a.id]
 }
 
