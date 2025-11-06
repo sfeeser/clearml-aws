06280a736ec2de6268aaf79de2e66d935a638c23
Stuart Feeser <sfeeser@gmail.com>
2025-11-06 12:38:28 -0500

    re-added install terraform steps

---
+++ terraform/clearml-aks/main.tf
@@ -0,0 +1,122 @@
+i# main.tf — FULLY SELF-CONTAINED AZURE AKS TERRAFORM
+
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------------
+# Variables
+# -------------------------------
+variable "location" { default = "eastus" }
+variable "cluster_name" { default = "clearml-dev" }
+variable "kubernetes_version" { default = "1.30" }
+variable "worker_node_count" { default = 2 }
+variable "ssh_public_key" { default = "~/.ssh/id_rsa.pub" }
+
+# -------------------------------
+# Resource Group
+# -------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.cluster_name}-rg"
+  location = var.location
+}
+
+# -------------------------------
+# VNet + Subnet
+# -------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.cluster_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "${var.cluster_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# -------------------------------
+# NSG: Allow SSH (22)
+# -------------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.cluster_name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------------
+# AKS Cluster
+# -------------------------------
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+
+  default_node_pool {
+    name                = "default"
+    node_count          = var.worker_node_count
+    vm_size             = "Standard_D4s_v5"  # 4 vCPU, 16 GB RAM
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+    os_disk_size_gb     = 64
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+# -------------------------------
+# Outputs
+# -------------------------------
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
